c719a53f8a60d24b436b84c07eb9910814053e7a
Dmitry <1@gmail.com>
2020-01-28 23:24:09 +0300

    Updates

---
+++ precache-manifest.4f6e387a134eed5eae967118a571c54b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23d0dda3e5a56a048a39d3d0b8c661e",
+    "url": "/react_app_social_network/index.html"
+  },
+  {
+    "revision": "9dc0ffa64fac06dd7ea0",
+    "url": "/react_app_social_network/static/css/3.444311a7.chunk.css"
+  },
+  {
+    "revision": "d851cf7afa474425532a",
+    "url": "/react_app_social_network/static/css/4.185615d5.chunk.css"
+  },
+  {
+    "revision": "ba58f783383dcfbdad8b",
+    "url": "/react_app_social_network/static/css/main.c4a27c17.chunk.css"
+  },
+  {
+    "revision": "e219707aa36900b6c093",
+    "url": "/react_app_social_network/static/js/2.568d1fd9.chunk.js"
+  },
+  {
+    "revision": "0e2bdae79ab87e95ca6fae12d3a18081",
+    "url": "/react_app_social_network/static/js/2.568d1fd9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9dc0ffa64fac06dd7ea0",
+    "url": "/react_app_social_network/static/js/3.17895a1f.chunk.js"
+  },
+  {
+    "revision": "d851cf7afa474425532a",
+    "url": "/react_app_social_network/static/js/4.4c49145d.chunk.js"
+  },
+  {
+    "revision": "ba58f783383dcfbdad8b",
+    "url": "/react_app_social_network/static/js/main.b8ff8b6a.chunk.js"
+  },
+  {
+    "revision": "6841af0ca2c62bcc044d",
+    "url": "/react_app_social_network/static/js/runtime-main.ba926f95.js"
+  },
+  {
+    "revision": "eded90f3c62d3fa459c18dee89b98fad",
+    "url": "/react_app_social_network/static/media/810.eded90f3.svg"
+  },
+  {
+    "revision": "7829623e088c045e07c0f31c07369c47",
+    "url": "/react_app_social_network/static/media/user-logo.7829623e.jpg"
+  }
+]);
